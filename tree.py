a956dc1220021b42c877c964ffdc73c00a481298
eprrio1 <elanaperriott@yahoo.com>
2023-04-13 02:55:29 +0000

    create numberOfLeaves method to Tree Class

--- tree.py
+++ tree.py
@@ -4,9 +4,21 @@ class Tree:
         self.left = left
         self.right = right
 
-    def is_a_left(self):
+    def is_a_leaf(self):
         if self.left == None and self.right == None:
             return True
         return False
 
-   
+    def numberOfLeaves(self):
+        if self.is_a_left() == True:
+            count += 1
+            return count
+
+        if self.left is not None:
+            return self.left.numebrOfLeaves(count)
+
+        if self.right is not None:
+            return self.right.numberOfLeaves(count)
+
+       
+        
