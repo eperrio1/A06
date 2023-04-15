87660ce589fb25459c049c8a4ba7b13cebb8d8c1
eperrio1 <elanaperriott@yahoo.com>
2023-04-15 19:03:04 +0000

    add test_numberOfLeaves to treeTest.py

--- treeTests.py
+++ treeTests.py
@@ -2,14 +2,26 @@ import unittest
 from tree import Tree  
 
 class treeTests(unittest.TestCase):
-    
     def test_is_leaf(self):
         proud_tree = Tree('Penny')
         proud_tree.left = Tree('Trudy')
-        proud_tree.right = Tree('Oscar')
+        proud_tree.right = Tree('Oscar')   
+    
         self.assertEqual(False, proud_tree.is_leaf())
         self.assertEqual(True, proud_tree.right.is_leaf())
         self.assertEqual(True, proud_tree.left.is_leaf())
+
+    def test_numberOfLeaves(self):
+        proud_tree = Tree('Penny')
+        proud_tree.left = Tree('Trudy')
+        proud_tree.right = Tree('Oscar')  
+        self.assertEqual(2, proud_tree.numberOfLeaves())
+        
+        annnie_tree = Tree("Annie")
+        self.assertEqual(1, annnie_tree.numberOfLeaves())
+    
+
+    
         
 
 
