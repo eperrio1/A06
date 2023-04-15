3b01a54c940ebc5ae5c7005251de605e209deec7
eperrio1 <elanaperriott@yahoo.com>
2023-04-15 17:24:34 +0000

    create output method for tree.py

--- tree.py
+++ tree.py
@@ -3,14 +3,19 @@ class Tree:
     def __init__(self, node, left=None, right=None):
         self.left = left
         self.right = right
+        self.node = node
 
-    def is_a_leaf(self):
-        if self.left == None and self.right == None:
+    # need to test
+    def is_leaf(self):
+        if self is None:
+            return True
+        if self.left is None and self.right is None:
             return True
         return False
 
+    # need to test
     def numberOfLeaves(self):
-        if self.is_a_left() == True:
+        if self.is_leaf() == True:
             count += 1
             return count
 
@@ -20,5 +25,15 @@ class Tree:
         if self.right is not None:
             return self.right.numberOfLeaves(count)
 
+    def output(self, indents):
+        display = '\t'* indents + self.node 
+        if self.left is not None:
+            display += '\n' + '\t' * (indents + 1) + self.left.output(indents + 1) 
+        if self.right is not None:
+            display += '\n' + '\t' * (indents + 1) + self.right.output(indents + 1) 
+        return display
+
+    def __str__(self):
+        return self.output(0)
        
         
