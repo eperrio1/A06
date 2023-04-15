f208dbf96171f3e7023862f0934c35487a4f13db
eperrio1 <elanaperriott@yahoo.com>
2023-04-15 17:42:12 +0000

    create family tree in main.py

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+from tree import Tree
 def main():
-    print("Hello World")
+    randomly_generate_tree = build_random_tree(6)
+
+    family_tree = Tree('Elana')
+    family_tree.left = Tree('Frederica')
+    family_tree.left.left = Tree('Erma')
+    family_tree.left.right = Tree('Louis')
+    family_tree.left.right.left = Tree("Golden")
+    
+    family_tree.right = Tree('Godfrey')
+    family_tree.right.left = Tree("Nita")
+    family_tree.right.left.left = Tree("Elly")
+    family_tree.right.left.right = Tree("Frank")
+    
+    family_tree.right.right = Tree("Tony")
+    family_tree.right.right.right = Tree("Godfrey")
+    family_tree.right.right.left = Tree("Bernice")
+    print(family_tree)
+
+def build_random_tree(m):
+    # for i in m:
+        # call method to grow tree 
+    # return tree 
+    return m
 
 if __name__ == "__main__":
     main()
