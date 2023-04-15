7dd6c5004e2c0ab43a5083c6ded1f08c3e171e7a
eperrio1 <elanaperriott@yahoo.com>
2023-04-15 18:59:53 +0000

    add numberOfLeaves method to tree.py

--- tree.py
+++ tree.py
@@ -5,7 +5,7 @@ class Tree:
         self.right = right
         self.node = node
 
-    # need to test
+
     def is_leaf(self):
         if self is None:
             return True
@@ -13,24 +13,24 @@ class Tree:
             return True
         return False
 
-    # need to test
     def numberOfLeaves(self):
         if self.is_leaf() == True:
-            count += 1
-            return count
+            return 1  
+       
+        if self is None:
+            return 0 
 
-        if self.left is not None:
-            return self.left.numebrOfLeaves(count)
+        if self.right is not None and self.left is not None:
+            return self.right.numberOfLeaves() + self.left.numberOfLeaves()
 
-        if self.right is not None:
-            return self.right.numberOfLeaves(count)
+
 
     def output(self, indents):
         display = '\t'* indents + self.node 
         if self.left is not None:
-            display += '\n' + '\t' * (indents + 1) + self.left.output(indents + 1) 
+            display += '\n' + '\t' * indents + self.left.output(indents + 1) 
         if self.right is not None:
-            display += '\n' + '\t' * (indents + 1) + self.right.output(indents + 1) 
+            display += '\n' + '\t' * indents + self.right.output(indents + 1) 
         return display
 
     def __str__(self):
